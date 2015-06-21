c2d31ef1af47d7e1894040b2ebfdb8c17b3a0e28
Erwann Mest <m@kud.io>
2015-06-21 23:43:41 +0200

    Some updates.

--- core/cli.js
+++ core/cli.js
@@ -1,7 +1,7 @@
 var shell = require('shelljs')
 var h     = require('./helper')
 
-npmInstall: function( app ) {
+var npmInstall = function( app ) {
   shell.exec('npm install -g ' + app)
 }
 
@@ -9,5 +9,6 @@ h.echoTitle('Installing commands...')
 
 npmInstall('imagemin')
 npmInstall('npm-check-updates')
+npmInstall('markdown-live')
 
 h.echoSuccess('Commands installed!')
