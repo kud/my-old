5b300c9d844b8b119afb5a3b2f4c889be6af92fa
Erwann Mest <m@kud.io>
2015-11-21 23:51:40 +0100

    Modify wallpaper.

--- core/settings.js
+++ core/settings.js
@@ -3,7 +3,7 @@ var h     = require('./helper')
 
 h.echoTitle('Setting computer...')
 
-var setItemProperty = 'set-itemproperty -path \\"HKCU:Control Panel\\Desktop\\" -name wallpaper -value \\"D:\\Sync\\low\\lib\\wallpapers\\DefaultDesktop.jpg\\"'
+var setItemProperty = 'set-itemproperty -path \\"HKCU:Control Panel\\Desktop\\" -name wallpaper -value \\"D:\\Sync\\low\\lib\\wallpaper.jpg\\"'
 
 shell.exec('@powershell -NoProfile -ExecutionPolicy unrestricted -Command "' + setItemProperty + '"')
 shell.exec('@powershell -NoProfile -ExecutionPolicy unrestricted -Command "RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters"') // Works itself but not via shell.exec :/
